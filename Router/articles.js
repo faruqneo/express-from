3679dc351663f097714d29ee302633b3eec35724
faruqneo <faruq@marmeto.com>
2019-02-05 12:53:35 +0530

    router is working

---
+++ Router/articles.js
@@ -0,0 +1,155 @@
+const express = require('express')
+const router = express.Router()
+
+//Connect with model
+let Article = require('../models/article');
+
+// middleware that is specific to this router
+router.use(function timeLog (req, res, next) {
+    console.log('Time: ', Date.now())
+    next()
+})
+
+
+//Getting Start
+router.get('/',function(req, res){
+    Article.find({},function(err, articles){
+        if(err)
+        {
+            console.log(err)
+        }
+        else{
+            res.render('index', {
+                articles: articles
+            });
+        }
+    });
+});
+
+
+//Add page 
+router.get('/add',function(req, res){
+    res.render('add');
+});
+
+
+//Insering new data
+router.route('/newarticles')
+
+//check for PUT method
+        .put(function(req, res){
+            res.render('restricted')
+        })
+
+//check for GET method
+    .get(function(req, res){
+        res.render('restricted')
+        })
+
+//check for POST method
+    .post( function(req, res){
+
+        req.checkBody('title', 'Title is required').notEmpty();
+        req.checkBody('author', 'Author is required').notEmpty();
+        req.checkBody('body', 'Body is required').notEmpty();
+
+        //Get error
+        let errors = req.validationErrors();
+
+        if(errors)
+        {   
+            
+            res.render('add',{
+                errors: errors
+            });
+        }
+        else
+        {
+            let article = new Article();
+            article.title = req.body.title;
+            article.author = req.body.author;
+            article.body = req.body.body;
+        
+            article.save(function(err){
+                if(err)
+                {
+                    console.log(err)
+                }
+                else
+                {
+                    req.flash('success', 'Article Added') // not working
+                    res.redirect('/')
+                }
+            });
+        }
+    })
+
+
+
+
+//Getting data
+router.get('/update/:id',function(req, res){
+    Article.findById(req.params.id, function(err, article){
+        res.render('edit',{
+            article: article
+        });
+    })
+});
+
+//Deleting data
+router.get('/update/article/delete/:id', function(req, res){
+    let id = {_id:req.params.id}
+    Article.deleteOne(id, function(err){
+        if(err)
+        {
+            console.log(err)
+        }
+        else
+        {
+            res.redirect('/')
+        }
+    })
+
+});
+
+//Getting data in form
+router.get('/update/article/view/:id',function(req, res){
+    Article.findById(req.params.id, function(err, article){
+        res.render('view',{
+            article: article
+        });
+    })
+});
+
+//Data updating
+router.route('/update/articles/:id')
+
+//checking for PUT
+    .put(function(req, res){
+        res.render('restricted')
+    })
+
+//checking for GET 
+    .get( function(req, res){
+        res.render('restricted')
+    })
+
+//checking for POST
+    .post( function(req, res){
+        let article = req.body;
+        
+        let id = {_id:req.params.id}
+        //console.log(req.params.id)
+        Article.updateOne(id, article,function(err){
+            if(err)
+            {
+                console.log(err)
+            }
+            else
+            {
+                res.redirect('/')
+            }
+        })
+    })
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express')
-const path = require('path')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
 const exphbs  = require('express-handlebars');
 const session = require('express-session')
-const flash = require('connect-flash')
+const articles = require('./Router/articles')
 
 //Init app
 const app = express()
@@ -19,19 +18,9 @@ db.once('open',function(){
     console.log("connected with mongodb")
 });
 
-
-//check connection error
-// db.on('error', function(){
-//     console.log(error)
-// });
-
-//Connect with model
-let Article = require('./models/article');
-
 //set view page
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -71,125 +60,9 @@ app.use(expressValidator({
     }
 }));
 
-//Getting Start
-app.get('/',function(req, res){
-    Article.find({},function(err, articles){
-        if(err)
-        {
-            console.log(err)
-        }
-        else{
-            res.render('index', {
-                articles: articles
-            });
-        }
-    });
-});
+//use router
+app.use('/', articles)
 
-//Add page 
-app.get('/add',function(req, res){
-    res.render('add');
-});
-
-//Insering new data
-app.post('/newarticles', function(req, res){
-
-    req.checkBody('title', 'Title is required').notEmpty();
-    req.checkBody('author', 'Author is required').notEmpty();
-    req.checkBody('body', 'Body is required').notEmpty();
-
-    //Get error
-    let errors = req.validationErrors();
-
-    if(errors)
-    {   
-        
-        res.render('add',{
-            errors: errors
-        });
-    }
-    else
-    {
-        let article = new Article();
-        article.title = req.body.title;
-        article.author = req.body.author;
-        article.body = req.body.body;
-    
-        article.save(function(err){
-            if(err)
-            {
-                console.log(err)
-            }
-            else
-            {
-                req.flash('success', 'Article Added') // not working
-                res.redirect('/')
-            }
-        });
-    }
-});
-
-//Securing post data
-app.get('/newarticles',function(req, res){
-    res.render('Warning')
-});
-
-//Getting data
-app.get('/update/:id',function(req, res){
-    Article.findById(req.params.id, function(err, article){
-        res.render('edit',{
-            article: article
-        });
-    })
-});
-
-//Deleting data
-app.get('/update/article/delete/:id', function(req, res){
-    let id = {_id:req.params.id}
-    Article.deleteOne(id, function(err){
-        if(err)
-        {
-            console.log(err)
-        }
-        else
-        {
-            res.redirect('/')
-        }
-    })
-
-});
-
-//Getting data in form
-app.get('/update/article/view/:id',function(req, res){
-    Article.findById(req.params.id, function(err, article){
-        res.render('view',{
-            article: article
-        });
-    })
-});
-
-//Data updating
-app.post('/update/articles/:id', function(req, res){
-    let article = req.body;
-    
-    let id = {_id:req.params.id}
-    //console.log(req.params.id)
-    Article.updateOne(id, article,function(err){
-        if(err)
-        {
-            console.log(err)
-        }
-        else
-        {
-            res.redirect('/')
-        }
-    })
-});
-
-//Updation security
-app.get('/update/articles/:id', function(req, res){
-    res.render('Warning')
-});
 
 //Server running
 app.listen(3000, function(req, res){
